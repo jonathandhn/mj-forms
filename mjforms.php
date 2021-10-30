d32a8f903ad926de6a0429e228e252feab8912ea
Jonathan DAHAN <getintouchwith@jonathanphoto.fr>
2021-10-30 12:39:06 +0200

    Clean mjforms.php

--- mjforms.php
+++ mjforms.php
@@ -8,22 +8,28 @@ Plugin Name: Mj Forms
 Plugin URI: https://jonathanphoto.fr
 Description: Manage your Mailjet subscribers with elementor forms. 
 Author: Jonathan DAHAN
-Version: 1.0.0
+Version: 1.0.1
 Author URI: https://jonathanphoto.fr
 */
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined('ABSPATH'))
+{
+    exit; // Exit if accessed directly
+    
 }
-define( 'MJ_FORM_PATH', plugin_dir_path( __FILE__ ) );
+define('MJ_FORM_PATH', plugin_dir_path(__FILE__));
 
+add_action('elementor_pro/init', function ()
+{
+    // Here its safe to include our action class file
+    include_once (MJ_FORM_PATH . 'ActionSubscriber.php');
 
-add_action( 'elementor_pro/init', function() {
-	// Here its safe to include our action class file
-	include_once( MJ_FORM_PATH . 'ActionSubscriber.php');
+    // Instantiate the action class
+    $mailjet_action = new Mailjet_Action_After_Submit();
 
-	// Instantiate the action class
-	$mailjet_action = new Mailjet_Action_After_Submit();
-
-	// Register the action with form widget
-	\ElementorPro\Plugin::instance()->modules_manager->get_modules( 'forms' )->add_form_action( $mailjet_action->get_name(), $mailjet_action );
+    // Register the action with form widget
+    \ElementorPro\Plugin::instance()
+        ->modules_manager
+        ->get_modules('forms')
+        ->add_form_action($mailjet_action->get_name() , $mailjet_action);
 });
+
